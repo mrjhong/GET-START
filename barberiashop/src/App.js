f05d886bd63c4ac5a2d67fcb4f85c59ffcd3b893
yego0210 <diegotrujillo0210@gmail.com>
2021-11-02 22:30:46 -0500

    G-Start

---
+++ barberiashop/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import header from './componentes/header';
+
+
+
+function App() {
+
+
+
+  return (
+
+    <>
+      <h1>Aprendiendo</h1>
+
+      <header/>
+      
+      
+    
+    </>
+  )
+  
+
+}
+
+export default App;
---
+++ barberiashop/src/componentes/header.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const header = () => {
+    return (
+        <div>
+            <h1>Barberia</h1>
+            <h2>Diego Trujillo</h2>
+            
+            
+        </div>
+    )
+}
+
+export default header
